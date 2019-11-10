b6adb81c626089047d4ef9ca2e4fe8eb817434db
user <chubarkinilsur@gmail.com>
2019-11-10 22:28:04 +0300

    fixup remarks

--- src/test/java/stc21/MyHashMapTest.java
+++ src/test/java/stc21/MyHashMapTest.java
@@ -1,6 +1,7 @@
+package stc21;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Collection;
 import java.util.HashMap;
